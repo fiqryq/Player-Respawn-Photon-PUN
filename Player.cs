29392c0366b3ca23019557804e701340b69015ad
Fiqry choerudin <fiqrychoerudin@student.telkomuniversity.ac.id>
2020-11-29 20:16:40 +0700

    Init

---
+++ Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using System.IO;
+
+public class PlayerManager : MonoBehaviour
+{
+	PhotonView PV;
+
+	void Awake()
+	{
+		PV = GetComponent<PhotonView>();
+	}
+
+	void Start()
+	{
+		if (PV.IsMine)
+		{
+			CreateController();
+		}
+	}
+
+	void CreateController()
+	{
+		PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerController"), new Vector3(-39, -3, -97), Quaternion.identity);
+	}
+}
